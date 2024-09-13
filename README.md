7b241ef31cd041dce0828098a72ef1943b290a95
auraticabhi <abhijeetgupta989@gmail.com>
2024-09-13 15:36:49 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,23 @@
+# Wordly - Your Document Chat Companion!
+
+Built with Next.js, tRPC, TypeScript, Prisma & Tailwind
+
+![Project Image](https://utfs.io/f/5e5c267b-8970-435c-909c-dc0936082ce9-3doyhb.jpg)
+
+## Features
+
+- 🛠️ Complete SaaS Platform
+- 💻 Beautiful Landing Page & Pricing Page Included
+- 📄 A Beautiful And Highly Functional PDF Viewer
+- 🔄 Streaming API Responses in Real-Time
+- 🔒 Authentication Using Kinde
+- 🎨 Clean, Modern UI Using 'shadcn-ui'
+- 🚀 Optimistic UI Updates for a Great UX
+- ⚡ Infinite Message Loading for Performance
+- 📤 Intuitive Drag n’ Drop Uploads
+- ✨ Instant Loading States
+- 🔧 Modern Data Fetching Using tRPC & Zod
+- 🧠 LangChain for Infinite AI Memory
+- 🌲 Pinecone as our Vector Storage
+- 📊 Prisma as our ORM
+- 🔤 TypeScript for type safety
---
+++ src/lib/utils.ts
@@ -0,0 +1,60 @@
+import { type ClassValue, clsx } from 'clsx'
+import { Metadata } from 'next'
+import { twMerge } from 'tailwind-merge'
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs))
+}
+
+export function absoluteUrl(path: string) {
+  if (typeof window !== 'undefined') return path
+  if (process.env.VERCEL_URL)
+    return `https://${process.env.VERCEL_URL}${path}`
+  return `http://localhost:${
+    process.env.PORT ?? 3000
+  }${path}`
+}
+
+export function constructMetadata({
+  title = "Wordly - chat with your documents",
+  description = "Wordly is an AI SaaS platform that simplifies interacting with your PDF files.",
+  image = "/thumbnail.png",
+  icons = "/favicon.ico",
+  noIndex = false
+}: {
+  title?: string
+  description?: string
+  image?: string
+  icons?: string
+  noIndex?: boolean
+} = {}): Metadata {
+  return {
+    title,
+    description,
+    openGraph: {
+      title,
+      description,
+      images: [
+        {
+          url: image
+        }
+      ]
+    },
+    twitter: {
+      card: "summary_large_image",
+      title,
+      description,
+      images: [image],
+      creator: "@AuraticAbhi"
+    },
+    icons,
+    metadataBase: new URL('https://wordly-jet.vercel.app'),
+    themeColor: '#FFF',
+    ...(noIndex && {
+      robots: {
+        index: false,
+        follow: false
+      }
+    })
+  }
+}
